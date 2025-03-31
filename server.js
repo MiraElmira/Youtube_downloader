b9e485261c07e5d0b1529e3d7fe1a2c8caabcf94
unknown <mira.elmira1903@gmail.com>
2025-03-31 15:07:35 +0400

    ?

--- server.js
+++ server.js
@@ -5,47 +5,49 @@ const uuid = require("uuid");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
+const dotenv = require('dotenv')
+.config();
 
 app.use(cors());
 app.use(express.json());
-app.use("/Back/Videos", express.static(path.join(__dirname, "Videos")));  // Statik dosyalar için 'Videos' klasörünü sunuyoruz
+app.use("/Back/Videos", express.static(path.join(__dirname, "Videos")));  // Serving static files from 'Videos' directory
 
-// Video indirme işlemi
+// Video download process
 app.post("/download", (req, res) => {
   const videoUrl = req.body.url;
 
-  // Benzersiz dosya adı oluşturuyoruz
+  // Generating a unique file name
   const videoName = `${uuid.v4()}.mp4`;  
   const downloadPath = path.join(__dirname, "Videos", videoName);
 
-  // Python betiğini çalıştırarak video indiriyoruz
+  // Running Python script to download video
   exec(`python youtubeDownloader.py "${videoUrl}" "${downloadPath}"`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      console.error(`Execution error: ${error}`);
       console.error(`stderr: ${stderr}`);
-      res.status(500).send("Video indirme hatası!");
+      res.status(500).send("Video download error!");
       return;
     }
 
-    console.log(stdout);  // Çıktıyı konsola yazdırıyoruz
-    // Video başarılı bir şekilde indirildiyse, video URL'sini döndürüyoruz
+    console.log(stdout);  // Logging output to console
+    // If the video is successfully downloaded, return the video URL
     res.json({ videoUrl: `/Back/Videos/${videoName}` });
 
-    // 10 dakika sonra video dosyasını sil
+    // Delete video file after 5 minutes
     setTimeout(() => {
       if (fs.existsSync(downloadPath)) {
         fs.unlink(downloadPath, (err) => {
           if (err) {
-            console.error(`Dosya silme hatası: ${err}`);
+            console.error(`File deletion error: ${err}`);
           } else {
-            console.log(`Video dosyası başarıyla silindi: ${downloadPath}`);
+            console.log(`Video file successfully deleted: ${downloadPath}`);
           }
         });
       }
-    }, 10 * 60 * 1000); // 10 dakika = 600,000 ms
+    }, 5 * 60 * 1000); // 5 minutes = 300,000 ms
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server 5000 portunda çalışıyor...");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}...`);
 });
